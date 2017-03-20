ea757547a0bd6d03667582c50ff6e8d33b179317
pjcm <pjcrespo@ikasle.aeg.es>
2017-03-20 17:15:05 +0100

    Cl.02 - NPM Scripts vs Task Runners

---
+++ gulp-vs-npm/gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp')
+  sass = require('gulp-sass')
+
+
+gulp.task('sass', () => {
+  return gulp
+    .src('./style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('default', ['sass'], () => {
+  gulp.watch('./style.scss', ['sass'])
+})
