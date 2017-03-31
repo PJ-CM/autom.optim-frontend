f6f3297dbbc4d8f24f61aedc9b87fc9007bbfd03
pjcm <pjcrespo@ikasle.aeg.es>
2017-03-31 20:34:26 +0200

    Cl.03-04 - Automatización de Tareas - Automatización y optimización de proyecto Frontend - 2a

--- npm-workflows/npm-frontend-boilerplate/dist/index.php
+++ npm-workflows/npm-frontend-boilerplate/dist/index.php
@@ -1,4 +1,16 @@
-<!DOCTYPE html><html lang="es"><head><meta charset="UTF-8"><title>INDEX-PHP :: desde Proxy</title><link href="https://fonts.googleapis.com/css?family=Barrio" rel="stylesheet"><link rel="stylesheet" href="./css/style.css"></head><body> <?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>INDEX-PHP :: desde Proxy</title>
+	<link href="https://fonts.googleapis.com/css?family=Barrio" rel="stylesheet">
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<?php
 	$sumando_1 = 44;
 	$sumando_2 = 33;
-	 echo '<h1>Aloha Mundo :) ... ('.$sumando_1.' + '.$sumando_2.') = '.($sumando_1 + $sumando_2).'</h1>'; ?> <script src="./js/script.js"></script></body></html>
+	 echo '<h1>Aloha Mundo :) ... ('.$sumando_1.' + '.$sumando_2.') = '.($sumando_1 + $sumando_2).'</h1>'; ?>
+	<script src="./js/script.js"></script>
+</body>
+</html>
